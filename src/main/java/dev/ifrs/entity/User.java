f099414cc53926ba863d0e001826d6e13eea9a34
Francisco Wolfarth Mossi <francisco.mossi@cwi.com.br>
2022-12-07 19:59:35 -0300

    removing unused setters

--- src/main/java/dev/ifrs/entity/User.java
+++ src/main/java/dev/ifrs/entity/User.java
@@ -17,31 +17,15 @@ public class User {
         return this.name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public String getEmail() {
         return this.email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
     }
 
     public String getPassword() {
         return this.password;
     }
 
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
     public String getPasswordConfirmation() {
         return this.passwordConfirmation;
-    }
-
-    public void setPasswordConfirmation(String passwordConfirmation) {
-        this.passwordConfirmation = passwordConfirmation;
     }
 }
